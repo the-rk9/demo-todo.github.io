2a0abc3f3c4cd1592b1f7295a5e0ddd4d09542a2
Rohan Kashyap <optimistic.rohan.09@gmail.com>
2022-08-22 23:19:45 +0530

    Updates

---
+++ precache-manifest.234f40577be0b4824d872ceb553eb0f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72a6d58db3488009af94",
+    "url": "/demo-todo.github.io/static/css/main.3daf0a52.chunk.css"
+  },
+  {
+    "revision": "72a6d58db3488009af94",
+    "url": "/demo-todo.github.io/static/js/main.72a6d58d.chunk.js"
+  },
+  {
+    "revision": "546dafc18c5050b214b1",
+    "url": "/demo-todo.github.io/static/js/1.546dafc1.chunk.js"
+  },
+  {
+    "revision": "a39159f34d5398b5f0e9",
+    "url": "/demo-todo.github.io/static/js/2.a39159f3.chunk.js"
+  },
+  {
+    "revision": "1400e7dbf45500fc3bae",
+    "url": "/demo-todo.github.io/static/js/runtime~main.1400e7db.js"
+  },
+  {
+    "revision": "99de96be92d0129441962d493b353511",
+    "url": "/demo-todo.github.io/index.html"
+  }
+];
